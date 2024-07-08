7eb6d5a29968d4a3c80b3f804e92efe54afde208
Radhika Choutukuri <radhika.choutukuri@infomagnus.com>
2024-07-08 12:59:58 +0530

    added comment for main content

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
-// this is important 
+// this is important update to specicy
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
